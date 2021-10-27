548d545a9b3178e1dbf768290d8e6f6886ed6d4b
kaizer1v <kaizer1v@gmail.com>
2021-10-27 21:16:20 +0530

    translation bug fixes #6

--- read_ocr.py
+++ read_ocr.py
@@ -1,6 +1,8 @@
 import os
 
 def run_for_ocr(opt):
+  translation = False if not opt['config']['translation'] else True
+
   ## step 1 - run something to generate the poly.txt file
   print('Running ocr_vision.py file to generate poly.txt')
   os.system('python ocr_vision.py {} > bounds.txt'.format(opt['url']))
@@ -10,7 +12,7 @@ def run_for_ocr(opt):
   os.system('bash generate_ocrconfig.sh {} {} {}'.format(
     opt['state_code'].lower(),
     "auto,auto",
-    False
+    translation
   ))
   ## step 3 - run googlevision.py file
   print('running googlevision.py using ocrconfig.meta file for {}'.format(opt['state_code']))
