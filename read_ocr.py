1e3d32fbec1e5d5be529eefae82da9a4b04d4870
kaizer1v <kaizer1v@gmail.com>
2021-10-30 14:31:14 +0530

    check for start & end key explicitly

--- read_ocr.py
+++ read_ocr.py
@@ -2,8 +2,14 @@ import os
 
 def run_for_ocr(opt):
   translation = False
+  start_key = 'auto'
+  end_key = 'auto'
   if 'config' in opt:
-     translation = False if not opt['config']['translation'] else opt['config']['translation']
+    translation = False if not opt['config']['translation'] else opt['config']['translation']
+    if 'start_key' in opt['config']:
+      start_key = opt['config']['start_key']
+    if 'end_key' in opt['config']:
+      end_key = opt['config']['end_key']
 
   ## step 1 - run something to generate the poly.txt file
   print('Running ocr_vision.py file to generate poly.txt')
@@ -13,7 +19,7 @@ def run_for_ocr(opt):
   print('Generating ocrconfig.meta file for {}'.format(opt['state_code']))
   os.system('bash generate_ocrconfig.sh {} {} {}'.format(
     opt['state_code'].lower(),
-    "auto,auto",
+    "{},{}".format(start_key, end_key),
     translation
   ))
   ## step 3 - run googlevision.py file
