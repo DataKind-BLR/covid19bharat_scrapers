5e80a884ee95f409fd2ac8c60b5128f301aea9cc
kaizer1v <kaizer1v@gmail.com>
2021-11-02 17:54:16 +0530

    debugging ct statewise

--- statewise_get_data.py
+++ statewise_get_data.py
@@ -164,37 +164,38 @@ def ct_get_data(opt):
       confirmedFound = False
       recoveredFound = False
       deceasedFound = False
-      for ind, line in enumerate(linesArray):
-        if ind == 0:
-          districtDictionary['districtName'] = linesArray[ind].strip()
-        elif ind == 3:
-          districtDictionary['confirmed'] = int(linesArray[ind].strip())
-          confirmedFound = True
-        elif ind == 5:
-          districtDictionary['recovered'] = int(linesArray[ind].strip())
-          recoveredFound = True
-        elif ind == 10:
-          districtDictionary['deceased'] = int(linesArray[ind].strip())
-          deceasedFound = True
-
-      # for index, data in enumerate(linesArray):
-      #   if availableColumns[index].strip() == "2":
-      #     districtDictionary['districtName'] = data.strip()
-      #   if availableColumns[index].strip() == "4":
-      #     districtDictionary['confirmed'] = int(data.strip())
+      # for ind, line in enumerate(linesArray):
+      #   if ind == 0:
+      #     districtDictionary['districtName'] = linesArray[ind].strip()
+      #   elif ind == 3:
+      #     districtDictionary['confirmed'] = int(linesArray[ind].strip())
       #     confirmedFound = True
-      #   if availableColumns[index].strip() == "9":
-      #     districtDictionary['recovered'] = int(data.strip())
+      #   elif ind == 5:
+      #     districtDictionary['recovered'] = int(linesArray[ind].strip())
       #     recoveredFound = True
-      #   if availableColumns[index].strip() == "12":
-      #     districtDictionary['deceased'] += int(data.strip())
+      #   elif ind == 10:
+      #     districtDictionary['deceased'] = int(linesArray[ind].strip())
       #     deceasedFound = True
 
-      #print(districtDictionary)
+      for index, data in enumerate(linesArray):
+        print(linesArray[index])
+        if availableColumns[index].strip() == "2":
+          districtDictionary['districtName'] = data.strip()
+        if availableColumns[index].strip() == "4":
+          districtDictionary['confirmed'] = int(data.strip())
+          confirmedFound = True
+        if availableColumns[index].strip() == "9":
+          districtDictionary['recovered'] = int(data.strip())
+          recoveredFound = True
+        if availableColumns[index].strip() == "12":
+          districtDictionary['deceased'] += int(data.strip())
+          deceasedFound = True
+
       if recoveredFound == False or confirmedFound == False:
         print("--> Issue with {}".format(linesArray))
         continue
       districts_data.append(districtDictionary)
+
   upFile.close()
   return districts_data
 
