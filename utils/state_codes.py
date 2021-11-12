4823991d0b7e36989eb1644b83bd79d96b8c6c86
kaizer1v <kaizer1v@gmail.com>
2021-11-12 10:19:48 +0530

    utils not being used (use states.yaml instead)

--- utils/state_codes.py
+++
@@ -1,1 +0,0 @@
-state_codes = ['ap','an','ar','as','br','ch','ct','dd','dh','dn','ga','gj','hp','hr','jh','jk','ka','kl','la','mh','ml','mn','mp','mz','nl','or','pb','py','rj','sk','tn','tg','tr','up','ut','wb']
