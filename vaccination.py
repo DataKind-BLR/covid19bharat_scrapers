bfa04937c2e553ed4e84a0be1bf594229400b2ff
kaizer1v <kaizer1v@gmail.com>
2021-12-05 21:41:28 +0530

    fixed file paths & names

--- vaccination.py
+++ vaccination.py
@@ -8,7 +8,10 @@ import pandas as pd
 
 warnings.simplefilter(action='ignore')
 
-VACC_TXT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_outputs', 'vaccination.txt')
+VACC_STA = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_outputs', 'vaccination_state_level.txt')
+VACC_DST = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_outputs', 'vaccination_district_level.csv')
+VACC_DST_COWIN = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_outputs', 'vaccination_cowin_district_level.csv')
+
 STATES_YAML = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'states.yaml')
 DISTRICTS_DATA_SHEET = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTrt_V4yW0jd91chhz9BJZOgJtFrsaZEa_gPlrFfQToBuuNDDkn01w0K0GdnjCdklyzFz84A1hFbSUN/pub?gid=382746758&single=true&output=csv'
 
@@ -34,6 +37,7 @@ def get_vaccination(lookback=1):
 
     district_rows = []
     today_str = today.strftime('%d-%m-%Y')
+
     # run for every state
     for state_code in states_all:
         params = {
@@ -48,7 +52,7 @@ def get_vaccination(lookback=1):
         age_groups = state_data['vaccinationByAge'] if 'vaccinationByAge' in state_data else state_data['topBlock'].get('vaccination')
 
         print('printing for ', states_all[state_code].get('name'), '---> all districts')
-        with open(VACC_TXT, 'a') as file:
+        with open(VACC_STA, 'a') as file:
             datum = [
                 today_str, \
                 states_all[state_code].get('name'), \
@@ -87,11 +91,9 @@ def get_vaccination(lookback=1):
             except:
                 print(district_url)
                 print(resp)
-                import pdb
-                pdb.set_trace()
 
             print('printing for ', states_all[state_code].get('name'), '--->', district['title'])
-            with open(VACC_TXT, 'a') as file:
+            with open(VACC_STA, 'a') as file:
                 datum = {
                     'updated_at': today_str, \
                     'State': states_all[state_code].get('name'), \
@@ -121,14 +123,15 @@ def get_vaccination(lookback=1):
 
     print("Making districts data file")
     district_data = pd.DataFrame(district_rows).drop('updated_at', 1)
-    district_data.to_csv(os.path.join('_outputs', 'district_data_vaccines.csv'), index=False)
+    district_data.to_csv(VACC_DST_COWIN, index=False)
+
     print("Getting state-district mapping from google sheet")
     public_data = pd.read_csv(DISTRICTS_DATA_SHEET)
     state_dist_mapping = public_data[['State_Code', 'State', 'District']].drop(0, axis=0)
     merged_data = pd.merge(state_dist_mapping, district_data, on=['State', 'District'], how='left')
     merged_data.columns = pd.MultiIndex.from_tuples([('' if k in ('State_Code', 'State', 'District') else today_str, k) for k in merged_data.columns])
-    district_data_fpath = os.path.join('_outputs', 'merged_district_data.csv')
-    merged_data.to_csv(district_data_fpath, index=False)
-    print("District data is saved to: ", district_data_fpath)
+
+    merged_data.to_csv(VACC_DST, index=False)
+    print("District data is saved to: ", VACC_DST)
 
 get_vaccination()
