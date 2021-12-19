8d45fe9397334bf819e48e1ede7002ecc2ed4629
Bharath <bee.ricketyship@gmail.com>
2021-12-19 19:15:44 +0530

    Update ocr_vision.py

--- ocr_vision.py
+++ ocr_vision.py
@@ -35,7 +35,7 @@ def detect_text(path):
 
 
 if __name__ == "__main__":
-  os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(os.path.dirname(__file__), "visionapi.json")
+  os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "visionapi.json"
   # path to the image file to be scanned
   path = sys.argv[1]
   detect_text(path)
