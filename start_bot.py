93d53fca56457f0fbde3ad0663de3ecdaea570c4
kaizer1v <kaizer1v@gmail.com>
2022-01-05 12:23:35 +0530

    read tokens from os.environ

--- start_bot.py
+++ start_bot.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import os
 import time
 import json
@@ -11,9 +8,21 @@ from time import sleep
 from telegram_bot.entry import entry
 from telegram.error import NetworkError, Unauthorized
 
-COVID_BOT_TOKEN='2057487700:AAFsjn8Jc9QDw6q4cCk9YjL2Q6_7xJv3cKk'
-VISIONAPI_TOKEN='91c256a14c3b399081bbeac9348cf0627643184f'
 LIFESPAN = 1200 # How long the container exist
+
+try:
+    COVID_BOT_TOKEN = os.getenv('COVID_BOT_TOKEN')
+except KeyError:
+    logger.error("Bot credentials not found in environment")
+
+try:
+    # If the token is available in the environment, print it to a file
+    VISIONAPI_TOKEN = os.getenv('VISIONAPI_TOKEN')
+    # print("Creating visionapi.json at : " + os.path.dirname(os.path.realpath(__file__)))
+    # with open("visionapi.json", "w") as f:
+        # print(VISIONAPI_TOKEN, file=f)
+except KeyError:
+    logger.error("VisionAPI credentials not found in environment")
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -21,23 +30,6 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S',
 )
 logger = logging.getLogger('Start_Bot')
-
-# try:
-#     # COVID_BOT_TOKEN = os.environ["COVID_BOT_TOKEN"]
-#     COVID_BOT_TOKEN='2057487700:AAFsjn8Jc9QDw6q4cCk9YjL2Q6_7xJv3cKk'
-# except KeyError:
-#     logger.error("Bot credentials not found in environment")
-
-# try:
-#     # If the token is available in the environment, print it to a file
-#     VISIONAPI_TOKEN = os.environ["VISIONAPI_TOKEN"]
-#     # TODO: Find a better fix
-#     print("Creating visionapi.json at : " + os.path.dirname(os.path.realpath(__file__)))
-#     with open("visionapi.json", "w") as f:
-#         print(VISIONAPI_TOKEN, file=f)
-# except KeyError:
-#     logger.error("VisionAPI credentials not found in environment")
-
 
 def main():
     """Run the bot @datakind_covid19bharat_bot."""
@@ -54,8 +46,6 @@ def main():
     while True:
         try:
             logger.info('Bot is at your service, I am listening....')
-            import pdb
-            pdb.set_trace()
             for update in bot.get_updates(offset=update_id, timeout=10):
                 update_id = update.update_id + 1
                 logger.info(f"Update ID:{update_id}")
