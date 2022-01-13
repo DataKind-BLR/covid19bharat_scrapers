b4290ed9d464003bf0556e76e406bd358db6a736
rraman64 <rraman64@gmail.com>
2022-01-13 06:34:08 +0000

    Bugfix: BOT filepath mismatch for images

--- telegram_bot/entry.py
+++ telegram_bot/entry.py
@@ -156,7 +156,7 @@ def entry(bot, update):
                     image_path = os.path.join(DOWNLD_DIR, '{}.jpeg'.format(opt['state_code'].lower()))
 
                 image_file = bot.get_file(photo.file_id)
-                image_file.download()
+                image_file.download(image_path)
 
                 opt['url'] = image_path
                 bot.send_message(
