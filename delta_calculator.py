e07998956879dca4e24df5d331f83a6e08ce8941
kaizer1v <kaizer1v@gmail.com>
2022-01-30 19:27:16 +0530

    added delta calc for UT (exception)

--- delta_calculator.py
+++ delta_calculator.py
@@ -52,6 +52,92 @@ def format_df(opt, df):
     return result_df
 
 
+def ut_calculate_detlas(opt, live_data):
+    '''
+    This delta calculation is only for the state of UT. This has been added because UT pdf
+    cumulatives now provide `total since 1st Jan, 2022`.
+    '''
+    # 0. get meta info
+    meta_df = pd.read_csv(DELTA_MAPPING, sep=',', encoding='utf-8', header=None, names=['state_name', 'from_dist', 'to_dist'])
+    state_meta_df = meta_df[meta_df['state_name'] == opt['name']][[
+        'from_dist',
+        'to_dist'
+    ]].apply(lambda x: x.str.strip()) # strip whitespaces
+    dist_to_rename = state_meta_df.set_index('from_dist').to_dict().get('to_dist')
+
+    # 1. get cumulative until 31st Dec
+    dt_dec = datetime.date(2021, 12, 31)
+    api_df = pd.read_csv(API_DIST_TS)
+    dt_dec_str = dt_dec.strftime('%Y-%m-%d')
+    dec_df = api_df[
+        (api_df['State'] == opt['name']) &
+        (api_df['Date'] == dt_dec_str)
+    ].rename(columns={'Other': 'Migrated_Other'})
+    dec_df = dec_df[[
+        'District',
+        'Confirmed',
+        'Recovered',
+        'Deceased',
+        'Migrated_Other'
+    ]].set_index('District').sort_index(ascending=True)
+
+    # 2. get cumulative from 1st jan until today (live_data)
+    live_df = pd.DataFrame.from_dict(live_data)
+    live_df.rename(columns={
+        'confirmed': 'Confirmed',
+        'recovered': 'Recovered',
+        'deceased' : 'Deceased',
+        'migrated':  'Migrated_Other'
+    }, inplace=True)
+    live_df.replace(dist_to_rename, inplace=True)
+    live_df = live_df.set_index('districtName').sort_index(ascending=True)
+
+    # 3. calculate (#1) + (#2)
+    today_cum_df = dec_df + live_df
+
+    # 4. get cumulatives for (today - 1) i.e. until yesterday
+    dt_yest = datetime.date.today() - datetime.timedelta(days=1)
+    dt_yest_str = dt_yest.strftime('%Y-%m-%d')
+    yest_cum_df = api_df[
+        (api_df['State'] == opt['name']) &
+        (api_df['Date'] == dt_yest_str)
+    ].rename(columns={'Other': 'Migrated_Other'})
+    yest_cum_df = yest_cum_df[[
+        'District',
+        'Confirmed',
+        'Recovered',
+        'Deceased',
+        'Migrated_Other'
+    ]].set_index('District').sort_index(ascending=True)
+
+    # 5. calculate (#3) - (#4)
+    delta_df = today_cum_df - yest_cum_df
+
+    # 6. format data frame
+    delta_df.fillna(0, inplace=True)
+    delta_df = delta_df.astype(int).reset_index()
+    delta_df.rename(columns={
+        'districtName': 'District',
+        'index': 'District'
+    }, inplace=True)
+
+    # 7. drop rows with no deltas
+    delta_df = delta_df.drop(delta_df[delta_df['District'].str.contains('Total')].index)
+
+    return {
+        'delta_totals': {
+            'confirmed': delta_df['Confirmed'].sum(),
+            'recovered': delta_df['Recovered'].sum(),
+            'deceased': delta_df['Deceased'].sum(),
+            'migrated': delta_df['Migrated_Other'].sum()
+        },
+        'deltas': delta_df,
+        'api_state_data': today_cum_df.reset_index(),
+        'for_sheets': format_df(opt, delta_df)
+    }
+
+
+
 def calculate_deltas(opt, live_data, dt=datetime.date.today()):
     '''
     Calculate difference b/w current data vs API/latest data and return deltas
@@ -63,6 +149,10 @@ def calculate_deltas(opt, live_data, dt=datetime.date.today()):
 
     :returns: <pd.DataFrame> - calculated difference dataframe
     '''
+
+    # 0. exception for UT delta calculation
+    if opt['state_code'] == 'UT':
+        return ut_calculate_detlas(opt, live_data)
 
     # 1. get updated API data & filter for selected state & sort
     if dt != datetime.date.today():
