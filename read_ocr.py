e578a73a2315b7ee0e567d06f7cb1c18add35b2a
kaizer1v <kaizer1v@gmail.com>
2022-04-24 16:29:07 +0530

    order hierarchy

--- read_ocr.py
+++ read_ocr.py
@@ -1,9 +1,9 @@
-from contextlib import redirect_stdout
 import io
 import os
-
 import googlevision
 import ocr_vision
+from contextlib import redirect_stdout
+
 
 python_cmd = 'python'
 if os.path.exists('use_venv_for_cmd'):
@@ -13,6 +13,7 @@ def run_for_ocr(opt):
     translation = False
     start_key = 'auto'
     end_key = 'auto'
+
     if 'config' in opt:
         translation = False if not opt['config']['translation'] else opt['config']['translation']
         if 'start_key' in opt['config']:
@@ -22,16 +23,16 @@ def run_for_ocr(opt):
 
     ## step 1 - run something to generate the poly.txt file
     print('Running ocr_vision.py file to generate _outputs/poly.txt')
-    
+
     f = io.StringIO()
     with redirect_stdout(f):
-      ocr_vision.run(opt["url"])
+      ocr_vision.run(opt['url'])
     result = f.getvalue()
-    
-    with open("_outputs/bounds.txt", "w") as f:
+
+    with open('_outputs/bounds.txt', 'w') as f:
       f.write(result)
-      f.close()  
-    
+      f.close()
+
     # os.system('{} ocr_vision.py {} > _outputs/bounds.txt'.format(python_cmd, opt['url']))
 
     ## step 2 - generate ocrconfig.meta file for that state (this overwrites previous file)
@@ -41,8 +42,8 @@ def run_for_ocr(opt):
         '{},{}'.format(start_key, end_key),
         translation
     ))
-    
+
     ## step 3 - run googlevision.py file
     print('running googlevision.py using ocrconfig.meta file for {}'.format(opt['state_code']))
-    googlevision.main(config_file="_outputs/ocrconfig.meta", file_name=opt["url"])
-    #os.system('{} googlevision.py _outputs/ocrconfig.meta {}'.format(python_cmd, opt['url']))
+    googlevision.main(config_file='_outputs/ocrconfig.meta', file_name=opt['url'])
+    # os.system('{} googlevision.py _outputs/ocrconfig.meta {}'.format(python_cmd, opt['url']))
