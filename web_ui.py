9f80a4982e3dac3e34f3fc55cfc338d1b7240d1e
kaizer1v <kaizer1v@gmail.com>
2022-04-25 21:22:37 +0530

    clean

--- web_ui.py
+++ web_ui.py
@@ -1,16 +1,13 @@
 import io
 import os
 import re
-import yaml
 import uvicorn
-# import argparse
 import scrapers
 import traceback
 import vaccination
 
-# from pathlib import Path
-from starlette.routing import Route
 from datetime import datetime, date
+from starlette.routing import Route
 from contextlib import redirect_stdout
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
@@ -19,193 +16,147 @@ from starlette.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory='templates')
 states_all = scrapers.states_all
-INPUTS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_inputs')
-OUTPUTS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_outputs')
-OUTPUT_TXT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '_outputs', 'output.txt')
-
-
-def clear_output_file():
-    '''
-    Given a dir, clear all file contents
-    '''
-    for path in os.listdir('_outputs'):
-        with open('_outputs/' + path, 'w') as f:
-          f.write('')
-          f.close()
-
 
 async def write_file(file_obj):
-    '''
-    Saves an uploaded file into `_inputs` directory
-    '''
-    # Path('web_ui_uploads').mkdir(parents=True, exist_ok=True)
-    contents = await file_obj.read()
-    # save_file_name = datetime.utcnow().isoformat() + '-' + file_obj.filename
-    save_file_name = 'web_ui' + file_obj.filename
-    save_file_name = os.path.join(INPUTS_DIR, save_file_name)
-
-    with open(save_file_name, 'wb') as f:
-        f.write(contents)
-        f.close()
-
-    return save_file_name
-  
+  contents = await file_obj.read()
+  save_file_name = datetime.utcnow().isoformat() + '-' + file_obj.filename
+  save_file_name = os.path.join(scrapers.INPUTS_DIR, save_file_name)
+  with open(save_file_name, 'wb') as f:
+    f.write(contents)
+    f.close()
+  return save_file_name
 
 async def homepage(request):
-    '''
-    Render the home page AND handle the POST request to provide ouput on the homepage
-    '''
-    available_state_codes = list(states_all.keys())
+  available_state_codes = list(states_all.keys())
 
-    ## If a form has been submitted from the page
-    if request.method == 'POST':
-        # clear_output_file()
+  if request.method == 'POST':
 
+    try:
+      form = await request.form()
+
+      url = None
+      if form.get('file'):
+        url = await write_file(form['file'])
+
+      skip_output = False
+      output_txt = form.get('output_txt')
+
+      if output_txt:
+        with open(os.path.join(scrapers.OUTPUTS_DIR, 'output.txt'),'w') as f:
+          f.write(output_txt.strip(' ').strip('\n'))
+          f.close()
+        skip_output = True
+
+      args = {
+        'state_code': form.get('state_code'),
+        'page': form.get('page'),
+        'skip_output': skip_output,
+        'type': form.get('type'),
+        'url': url if form.get('type') in ['pdf','image'] else None
+      }
+
+      f = io.StringIO()
+      with redirect_stdout(f):
+        result = scrapers.run(args)
+      delta = f.getvalue()
+
+      output_correction = None
+      if 'needs_correction' in result and result['needs_correction'] == True:
         try:
-            form = await request.form()
-            url = None
+          with open(os.path.join(scrapers.OUTPUTS_DIR, 'output.txt')) as f:
+            output_correction = f.read()
+            f.close()
+        except:
+          pass
 
-            if form.get('file'):
-                url = await write_file(form['file'])
 
-            skip_output = False
-            output_txt = form.get('output_txt')
-
-            if output_txt:
-                with open(OUTPUT_TXT, 'w') as f:
-                    f.write(output_txt.strip(' ').strip('\n'))
-                    f.close()
-                skip_output = True
-
-            args = {
-                'state_code': form.get('state_code'),
-                'page': form.get('page', None),
-                'skip_output': skip_output,
-                'type': form.get('type'),
-                'url': url if form.get('type') in ['pdf', 'image'] else None
-                # 'verbose': False,
-                # 'delta_date': date.today()
-            }
-            print('<<<<<<<<<<<<<<<<<', args)
-            result = scrapers.run(args)
-
-            print('--'*20)
-            print(result)
-            print('--'*20)
-
-            # f = io.StringIO()
-            # with redirect_stdout(f):
-            #     result = scrapers.run(args)
-
-            # delta = f.getvalue()
-            output_correction = None
-
-            # if result and type(result) is dict and result.get('needs_correction'):
-            if 'needs_correction' in result and result['needs_correction'] == True:
-                try:
-                    with open(OUTPUT_TXT) as f:
-                        output_correction = f.read()
-                        f.close()
-                except:
-                    pass
-
-            # clear_output_file()
-
-            return templates.TemplateResponse('index.html', {
-                'request': request,
-                # 'output': delta,
-                'output': result,
-                'available_state_codes': available_state_codes,
-                'output_correction': output_correction
-            })
-
-        ## If scraper doesn't run as expected, show error on home page
-        except Exception as e:
-            output = traceback.format_exc()
-            # clear_output_file()
-
-            return templates.TemplateResponse('index.html', {
-                'request': request,
-                'output': output,
-                'available_state_codes': available_state_codes,
-                'output_correction': None
-            })
-
-    ## Default render the home page with following data
-    return templates.TemplateResponse('index.html', {
+      return templates.TemplateResponse('index.html', {
         'request': request,
-        'output': '',
+        'output': delta,
+        'available_state_codes': available_state_codes,
+        'output_correction': output_correction
+      })
+
+    except Exception as e:
+      output = traceback.format_exc()
+      return templates.TemplateResponse('index.html', {
+        'request': request,
+        'output': output,
         'available_state_codes': available_state_codes,
         'output_correction': None
-    })
+      })
+
+  return templates.TemplateResponse('index.html', {
+    'request': request,
+    'output': '',
+    'available_state_codes': available_state_codes,
+    'output_correction': None
+  })
 
 
 async def vaccination_route(request):
-    output = ''
-    f = io.StringIO()
-    is_mohfw = False
-  
-    with redirect_stdout(f):
-        try:
-            if request.method=='POST':
-                form = await request.form()
-                fn_map = {
-                    'cowin_state': vaccination.get_cowin_state,
-                    'cowin_district': vaccination.get_cowin_district,
-                    'mohfw_state': vaccination.get_mohfw_state
-                }
+  output = ''
+  f = io.StringIO()
+  is_mohfw = False
 
-                vacc_src = form.get('source').lower()
-                is_mohfw = vacc_src=='mohfw_state'
+  with redirect_stdout(f):
+    try:
+      if request.method=='POST':
+        form = await request.form()
+        fn_map = {
+          'cowin_state': vaccination.get_cowin_state,
+          'cowin_district': vaccination.get_cowin_district,
+          'mohfw_state': vaccination.get_mohfw_state
+        }
+        vacc_src = form.get('source').lower()
+        is_mohfw = vacc_src=='mohfw_state'
 
-                if os.path.exists('_outputs/vaccination_state_level.txt'):
-                    os.remove('_outputs/vaccination_state_level.txt')
+        if os.path.exists('_outputs/vaccination_state_level.txt'):
+          os.remove('_outputs/vaccination_state_level.txt')
 
-                if os.path.exists('_outputs/vaccination_district_level.csv'):
-                    os.remove('_outputs/vaccination_district_level.csv')
+        if os.path.exists('_outputs/vaccination_district_level.csv'):
+          os.remove('_outputs/vaccination_district_level.csv')
 
-                state_codes = None
-                if form.get('state_codes'):
-                    state_codes = list(map(lambda sc: sc.lower().strip(), form.get('state_codes').split(',')))
+        state_codes = None
+        if form.get('state_codes'):
+          state_codes = list(map(lambda sc: sc.lower().strip(), form.get('state_codes').split(',')))
 
-                from_date = datetime(*list(map(int, form.get('from_date').split('-')))) if form.get('from_date') else datetime.today()
+        from_date = datetime(*list(map(int, form.get('from_date').split('-')))) if form.get('from_date') else datetime.today()
 
-                to_date = datetime(*list(map(int, form.get('to_date').split('-')))) if form.get('to_date') else None
-                if to_date is None or to_date <= from_date:
-                    to_date = from_date
+        to_date = datetime(*list(map(int, form.get('to_date').split('-')))) if form.get('to_date') else None
+        if to_date is None or to_date <= from_date:
+            to_date = from_date
 
-                fn_map[vacc_src](from_date, to_date, state_codes)
+        fn_map[vacc_src](from_date, to_date, state_codes)
 
-                if vacc_src=='cowin_state':
-                    with open('_outputs/vaccination_state_level.txt') as ff:
-                        output += ff.read() + '\n'
-                        ff.close()
-                if vacc_src=='cowin_district':
-                    with open('_outputs/vaccination_district_level.csv') as ff:
-                        output += ff.read() + '\n'
-                        ff.close()
+        if vacc_src=='cowin_state':
+          with open('_outputs/vaccination_state_level.txt') as ff:
+            output += ff.read() + '\n'
+            ff.close()
+        if vacc_src=='cowin_district':
+          with open('_outputs/vaccination_district_level.csv') as ff:
+            output += ff.read() + '\n'
+            ff.close()
+    except Exception as e:
+      output += traceback.format_exc() + '\n'
 
-        except Exception as e:
-            output += traceback.format_exc() + '\n'
-  
-    if is_mohfw:
-        output = f.getvalue() + '\n' + output
-        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+  if is_mohfw:
+    output = f.getvalue() + '\n' + output
+  ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+  return templates.TemplateResponse('vaccination.html', {
+    'request': request,
+    'output': ansi_escape.sub('', output)})
 
-    return templates.TemplateResponse('vaccination.html', {
-        'request': request,
-        'output': ansi_escape.sub('', output)
-    })
-    
+
 
 async def state_details(request):
-    return JSONResponse(states_all.get(request.path_params['state_code'],{}))
+  return JSONResponse(states_all.get(request.path_params['state_code'],{}))
 
-app = Starlette(debug=True, routes=[
+
+if __name__ == '__main__':
+  app = Starlette(debug=True, routes=[
     Route('/', homepage, methods=['GET', 'POST']),
     Route('/vaccination', vaccination_route, methods=['GET', 'POST']),
     Route('/state_details/{state_code}', state_details)
-])
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8005)
+  ])
+  uvicorn.run(app, host='0.0.0.0', port=8005)
